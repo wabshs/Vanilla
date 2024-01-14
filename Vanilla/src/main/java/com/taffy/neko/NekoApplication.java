aa0a98df5aa20230fc205b9492c562d40182e5ab
Nino <840654613@qq.com>
2024-01-14 20:36:47 +0800

    [add] 整合jasypt实现配置类敏感数据加密

--- Vanilla/src/main/java/com/taffy/neko/NekoApplication.java
+++ Vanilla/src/main/java/com/taffy/neko/NekoApplication.java
@@ -1,11 +1,13 @@
 package com.taffy.neko;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @MapperScan("com.taffy.neko.mapper")
+@EnableEncryptableProperties
 public class NekoApplication {
 
     public static void main(String[] args) {
